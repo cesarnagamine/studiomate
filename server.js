28e6831a4f5b7faf670d3f0edf55688f1c6c5ac6
unknown <cesarnagamine@gmail.com>
2020-07-14 01:19:43 -0500

    yeah

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-const publicPath = path.join(__dirname, 'client/public');
-app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
