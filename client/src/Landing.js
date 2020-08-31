6931803f9fa28e2c9456c10c0ecd788f6050e28f
unknown <cesarnagamine@gmail.com>
2020-08-30 19:14:38 -0500

    make it better

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -8,7 +8,7 @@ import ReactStars from "react-rating-stars-component";
 
 const Exercise = props => (
   <div className='inner'>
-    <h6>@{props.exercise.name}:</h6>  <ReactStars
+    <h5>@{props.exercise.name}:</h5>  <ReactStars
                 type="number"
                 name="rate"
                 id="rate"
